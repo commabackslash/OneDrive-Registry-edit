08f0ed2c4039b346647cd661d039a567a7fa7509
commabackslash <93020842+commabackslash@users.noreply.github.com>
2023-02-21 18:03:37 -0500

    lets start laying it out

--- scripts/add_printers.py
+++ scripts/add_printers.py
@@ -1,2 +1,19 @@
 #TODO later , doing research on libraries and such.
 #putting file here as placeholder
+
+import os
+import sys
+import pywin
+
+# [] Grabbing user information
+device_username = str(os.getlogin())
+user_directory = ("C:\\Users\\" + device_username)
+email_format = ("")
+print("[*] Logged in as user " + "'" + device_username + "'")
+print("[*] Microsoft user " + "'" + device_username + email_format + "'")
+print("_" * 20)
+print("")
+
+def SearchAD():
+    print("helloworld")
+
