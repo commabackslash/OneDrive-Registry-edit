c1cf1c97c3755191a1d00ac80b1d39d5ea569043
commabackslash <93020842+commabackslash@users.noreply.github.com>
2023-02-23 16:38:36 -0500

    :100: we are getting somewhere fr

--- scripts/add_printers.py
+++ scripts/add_printers.py
@@ -1,19 +1,24 @@
-#TODO later , doing research on libraries and such.
-#putting file here as placeholder
+# BASIC AD ENUMERATION
 
-import os
 import sys
-import pywin
+from ldap3 import Server, Connection, ALL, NTLM, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES, AUTO_BIND_NO_TLS, SUBTREE
+from ldap3.core.exceptions import LDAPCursorError
 
-# [] Grabbing user information
-device_username = str(os.getlogin())
-user_directory = ("C:\\Users\\" + device_username)
-email_format = ("")
-print("[*] Logged in as user " + "'" + device_username + "'")
-print("[*] Microsoft user " + "'" + device_username + email_format + "'")
-print("_" * 20)
-print("")
+server_name = ''
+domain_name = ''
+user_name = ''
+password = ''
 
-def SearchAD():
-    print("helloworld")
+format_string = '{:25} {:>6} {:19} {:19} {}'
+print(format_string.format('User', 'Logins', 'Last Login', 'Expires', 'Description'))
 
+server = Server(server_name, get_info=ALL)
+conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
+conn.search('dc={},dc=local'.format(domain_name), '(objectclass=person)', attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+for e in conn.entries:
+    try:
+        desc = e.description
+    except LDAPCursorError:
+        desc = ""
+
+print(format_string.format(str(e.name), str(e.logonCount), str(e.lastLogon)[:19], str(e.accountExpires)[:19], desc))
